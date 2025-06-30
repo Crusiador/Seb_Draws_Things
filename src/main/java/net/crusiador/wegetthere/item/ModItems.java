c1ab75cc1ab7f81eac72e4d18df709628ed61bcc
Crusiador <cp.gaming@gmx.ch>
2025-06-30 16:32:18 +0200

    do it

---
+++ src/main/java/net/crusiador/wegetthere/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.crusiador.wegetthere.item;
+
+import net.crusiador.wegetthere.wegetthere;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, wegetthere.MOD_ID);
+
+    public static final RegistryObject<Item> MARTONSZEUGNIS = ITEMS.register("martonszeugnis",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAFAELSZEUGNIS = ITEMS.register("rafaelszeugnis",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
