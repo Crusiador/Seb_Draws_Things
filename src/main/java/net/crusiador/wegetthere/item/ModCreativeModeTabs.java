a0b1ae76979a9af2be7431ba98cef714d8d55a1e
Crusiador <cp.gaming@gmx.ch>
2025-07-01 21:51:17 +0200

    Creative Mode Tabs

--- src/main/java/net/crusiador/wegetthere/item/ModCreativeModeTabs.java
+++ src/main/java/net/crusiador/wegetthere/item/ModCreativeModeTabs.java
@@ -1,4 +1,31 @@
 package net.crusiador.wegetthere.item;
 
+import net.crusiador.wegetthere.block.ModBlocks;
+import net.crusiador.wegetthere.wegetthere;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, wegetthere.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> ALEXANDRITE_ITEMS_TAB = CREATIVE_MODE_TABS.register("alexanderite_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ALEXANDRITE.get()))
+                    .title(Component.translatable("creativetab.tutorialmod.alexandrite_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.RAW_ALEXANDRITE.get());
+                        output.accept(ModItems.ALEXANDRITE.get());
+                        output.accept(ModBlocks.RAW_ALEXANDRITE_BlocK.get());
+                        output.accept(ModBlocks.ALEXANDRITE_BlocK.get());
+
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
--- src/main/java/net/crusiador/wegetthere/wegetthere.java
+++ src/main/java/net/crusiador/wegetthere/wegetthere.java
@@ -2,6 +2,7 @@ package net.crusiador.wegetthere;
 
 import com.mojang.logging.LogUtils;
 import net.crusiador.wegetthere.block.ModBlocks;
+import net.crusiador.wegetthere.item.ModCreativeModeTabs;
 import net.crusiador.wegetthere.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -32,6 +33,8 @@ public class wegetthere {
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
