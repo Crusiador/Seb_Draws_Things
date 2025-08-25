3de6eb5704e07b2677c9c643bbd4f2396f844c7d
crusiador <cp.gaming@gmx.ch>
2025-08-25 11:38:29 +0200

    Sebformer Rework

--- src/main/java/net/crusiador/wegetthere/block/custom/SebFormer.java
+++ src/main/java/net/crusiador/wegetthere/block/custom/SebFormer.java
@@ -13,7 +13,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
